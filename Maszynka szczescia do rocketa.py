a4da5a5ad234bac643f5fbeb5e9a46f30dfa4561
Under the Pebble Studio <49347186+PodkamykStudio@users.noreply.github.com>
2021-04-02 15:41:58 +0200

    Add files via upload

---
+++ Maszynka szczescia do rocketa.py
@@ -0,0 +1,61 @@
+import random
+
+while True:
+    i = random.randint(0,10)
+    addition = random.randint(1,4)
+    if i == 3 or i == 6 or i == 9: #30%
+        print("\nranked byczku")
+        print(f"wylosowana liczba to {i}")
+        kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+        if kontynuacja == 0:
+            continue
+        else:
+            break
+    elif i == 1 or i == 5 or i == 7: #30%
+        print("\njakis dodatkowy byczku")
+        print(f"wylosowana liczba to {i}")
+        answer = int(input("\nCzy wylosować tryb dodatkowy?  0-tak 1-nie"))
+        if answer == 0:
+            if addition == 1:
+                print("Rumble\n")
+                kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+                if kontynuacja == 0:
+                    continue
+                else:
+                    break
+
+            elif addition == 2:
+                print("Dropshot")
+                kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+                if kontynuacja == 0:
+                    continue
+                else:
+                    break
+            elif addition == 3:
+                print("koszykówka")
+                kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+                if kontynuacja == 0:
+                    continue
+                else:
+                    break
+            elif addition == 4:
+                print("hokej")
+                kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+                if kontynuacja == 0:
+                    continue
+                else:
+                    break
+        else:
+            kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+            if kontynuacja == 0:
+                continue
+            else:
+                break
+    else: #40%
+        print("normal byczku")
+        print(f"wylosowana liczba to {i}")
+        kontynuacja = int(input("\nKontynuować losowanie byczku? 0-tak 1-nie"))
+        if kontynuacja == 0:
+            continue
+        else:
+            break
